180dfdb54e46f81e5a2f1ebf63e2e8295696e852
Angel Romero <mrangelrcd@gmail.com>
2023-01-17 18:32:20 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Square from './components/Square'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Square></Square>
+    </>
+  )
+}
+
+export default App
